891f57c0ecfeffbf61a5f16f48541f046c05cafd
sinyoung3095 <sinyoung3095@gmail.com>
2025-11-03 16:53:14 +0900

    작업중

--- src/main/java/com/example/kok/service/ExperienceNoticeServiceImpl.java
+++ src/main/java/com/example/kok/service/ExperienceNoticeServiceImpl.java
@@ -79,21 +79,21 @@ public class ExperienceNoticeServiceImpl implements ExperienceNoticeService {
         String jobName= experienceNoticeDAO.findJobNameByID(id);
         result.setJobName(jobName);
         LocalDate endDate = LocalDate.parse(result.getExperienceNoticeEndDate());
-            LocalDate today = LocalDate.now();
-            if (!endDate.isBefore(today)) {
-                long days = ChronoUnit.DAYS.between(today, endDate);
-                result.setRemainingDays(days);
-            } else {
-                result.setRemainingDays(0L); // endDate보다 today가 이전일 경우 0
-            }
-            fileService.findFileByCompanyId(result.getCompanyId())
-                    .ifPresentOrElse(fileDTO -> {
-                        result.setFileName(fileDTO.getFileName());
-                        result.setFilePath(fileDTO.getFilePath());
-                    }, ()->{
-                        result.setFileName("image.png");
-                        result.setFilePath("");
-                    });
+        LocalDate today = LocalDate.now();
+        if (!endDate.isBefore(today)) {
+            long days = ChronoUnit.DAYS.between(today, endDate);
+            result.setRemainingDays(days);
+        } else {
+            result.setRemainingDays(0L); // endDate보다 today가 이전일 경우 0
+        }
+        fileService.findFileByCompanyId(result.getCompanyId())
+                .ifPresentOrElse(fileDTO -> {
+                    result.setFileName(fileDTO.getFileName());
+                    result.setFilePath(fileDTO.getFilePath());
+                }, ()->{
+                    result.setFileName("image.png");
+                    result.setFilePath("");
+                });
         return result;
     }
 
